51e78611b8eae64e51529be2918294c38a467c96
Cyril PASCAL <cyril.pascal_github@m4x.org>
2019-07-24 19:06:13 +0200

    Add deprecation for function typehints

---
+++ tests/Rules/Deprecations/TypeHintDeprecatedInFunctionSignatureRuleTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Deprecations;
+
+class TypeHintDeprecatedInFunctionSignatureRuleTest extends \PHPStan\Testing\RuleTestCase
+{
+
+	protected function getRule(): \PHPStan\Rules\Rule
+	{
+		$broker = $this->createBroker();
+
+		return new TypeHintDeprecatedInFunctionSignatureRule(new DeprecatedClassHelper($broker));
+	}
+
+	public function test(): void
+	{
+		require_once __DIR__ . '/data/typehint-function-deprecated-class-definition.php';
+		$this->analyse(
+			[__DIR__ . '/data/typehint-function-deprecated-class.php'],
+			[
+				['Parameter $property of function TypeHintDeprecatedInFunctionSignature\setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 8],
+				['Parameter $property2 of function TypeHintDeprecatedInFunctionSignature\setProperties() has typehint with deprecated interface TypeHintDeprecatedInFunctionSignature\DeprecatedInterface.', 8],
+				["Parameter \$property4 of function TypeHintDeprecatedInFunctionSignature\setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\VerboseDeprecatedProperty:\nI'll be back", 8],
+				['Parameter $property6 of function TypeHintDeprecatedInFunctionSignature\setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 8],
+				['Return type of function TypeHintDeprecatedInFunctionSignature\setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 8],
+			]
+		);
+	}
+
+}
