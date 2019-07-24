6bd7a15ae894af1a4d9d7c07b847d40ea6a4bf84
Cyril PASCAL <cyril.pascal_github@m4x.org>
2019-07-24 18:47:20 +0200

    Rename tests for typehint deprecation check in class method

--- tests/Rules/Deprecations/TypeHintDeprecatedInClassMethodSignatureRuleTest.php
+++ tests/Rules/Deprecations/TypeHintDeprecatedInClassMethodSignatureRuleTest.php
@@ -14,16 +14,18 @@ class TypeHintDeprecatedInClassMethodSignatureRuleTest extends \PHPStan\Testing\
 
 	public function test(): void
 	{
-		require_once __DIR__ . '/data/typehint-deprecated-class-definition.php';
+		require_once __DIR__ . '/data/typehint-class-method-deprecated-class-definition.php';
 		$this->analyse(
-			[__DIR__ . '/data/typehint-deprecated-class.php'],
+			[__DIR__ . '/data/typehint-class-method-deprecated-class.php'],
 			[
-				['Parameter $property of method TypeHintDeprecatedInFunctionSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 13],
-				['Parameter $property2 of method TypeHintDeprecatedInFunctionSignature\Foo::setProperties() has typehint with deprecated interface TypeHintDeprecatedInFunctionSignature\DeprecatedInterface.', 13],
-				["Parameter \$property4 of method TypeHintDeprecatedInFunctionSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\VerboseDeprecatedProperty:\nI'll be back", 13],
-				['Parameter $property6 of method TypeHintDeprecatedInFunctionSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 13],
-				['Return type of method TypeHintDeprecatedInFunctionSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 13],
-				['Parameter $property of method TypeHintDeprecatedInFunctionSignature\FooImplNoOverride::oops() has typehint with deprecated class TypeHintDeprecatedInFunctionSignature\DeprecatedProperty.', 50],
+				['Parameter $property of method TypeHintDeprecatedInClassMethodSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 13],
+				['Parameter $property2 of method TypeHintDeprecatedInClassMethodSignature\Foo::setProperties() has typehint with deprecated interface TypeHintDeprecatedInClassMethodSignature\DeprecatedInterface.', 13],
+				["Parameter \$property4 of method TypeHintDeprecatedInClassMethodSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\VerboseDeprecatedProperty:\nI'll be back", 13],
+				['Parameter $property6 of method TypeHintDeprecatedInClassMethodSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 13],
+				['Return type of method TypeHintDeprecatedInClassMethodSignature\Foo::setProperties() has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 13],
+				['Parameter $property of method TypeHintDeprecatedInClassMethodSignature\FooImplNoOverride::oops() has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 50],
+				['Parameter $property of method __construct() in anonymous class has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 71],
+				['Return type of method getProperty() in anonymous class has typehint with deprecated class TypeHintDeprecatedInClassMethodSignature\DeprecatedProperty.', 76],
 			]
 		);
 	}
