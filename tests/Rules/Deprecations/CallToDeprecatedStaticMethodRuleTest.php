d63a144a56302f2161b00727081884f1a6b1c8d0
Eirik S. Morland <eirik@morland.no>
2021-11-21 18:07:26 +0100

    Fix assertions for phpstan master

--- tests/Rules/Deprecations/CallToDeprecatedStaticMethodRuleTest.php
+++ tests/Rules/Deprecations/CallToDeprecatedStaticMethodRuleTest.php
@@ -26,6 +26,10 @@ class CallToDeprecatedStaticMethodRuleTest extends \PHPStan\Testing\RuleTestCase
 					6,
 				],
 				[
+					'Call to deprecated method deprecatedFoo() of class CheckDeprecatedStaticMethodCall\Bar.',
+					8,
+				],
+				[
 					'Call to deprecated method deprecatedFoo2() of class CheckDeprecatedStaticMethodCall\Foo.',
 					9,
 				],
@@ -56,10 +60,6 @@ class CallToDeprecatedStaticMethodRuleTest extends \PHPStan\Testing\RuleTestCase
 				[
 					"Call to deprecated method deprecatedWithDescription() of class CheckDeprecatedStaticMethodCall\Foo:\nThis is probably a singleton.",
 					16,
-				],
-				[
-					'Call to deprecated method deprecatedFoo() of class CheckDeprecatedStaticMethodCall\Foo.',
-					24,
 				],
 			]
 		);
