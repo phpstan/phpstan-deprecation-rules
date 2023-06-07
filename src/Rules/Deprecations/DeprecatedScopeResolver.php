ce6a85a1c2af4561b0fc2415b7f9712af3a4852b
Ondrej Mirtes <ondrej@mirtes.cz>
2023-06-07 11:48:32 +0200

    DeprecatedScopeResolver - description in PHPDoc

--- src/Rules/Deprecations/DeprecatedScopeResolver.php
+++ src/Rules/Deprecations/DeprecatedScopeResolver.php
@@ -4,7 +4,21 @@ namespace PHPStan\Rules\Deprecations;
 
 use PHPStan\Analyser\Scope;
 
-/** @api */
+/**
+ * This is the interface for custom deprecated scope resolvers.
+ *
+ * To register it in the configuration file use the `phpstan.deprecations.deprecatedScopeResolver` service tag:
+ *
+ * ```
+ * services:
+ * 	-
+ *		class: App\PHPStan\MyExtension
+ *		tags:
+ *			- phpstan.deprecations.deprecatedScopeResolver
+ * ```
+ *
+ * @api
+ */
 interface DeprecatedScopeResolver
 {
 
